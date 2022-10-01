ab0981a4cf7a338b178f835b0ad9fafe8687b00a
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-10-01 15:59:59 -0700

    Tweak styles

--- src/lib/islands/Interaction.svelte
+++ src/lib/islands/Interaction.svelte
@@ -9,9 +9,18 @@
 	});
 </script>
 
-{#if hydrated}
-	<p>I've been hydrated! 😋</p>
-{:else}
-	<p>So thirsty 😰</p>
-{/if}
-<Count />
+<div>
+	{#if hydrated}
+		<p>I've been hydrated! 😋</p>
+	{:else}
+		<p>So thirsty 😰</p>
+	{/if}
+	<Count />
+</div>
+
+<style>
+	div {
+		border: 2px dashed var(--primary);
+		padding: 1rem;
+	}
+</style>
