c2daa3dcf50c1440feb2648f94a1606582027b0f
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-10-02 19:42:57 -0700

    Prerender with adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
