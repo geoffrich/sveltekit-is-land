ec3f4732e84e968941c28ae491371385f1565386
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-10-02 20:16:50 -0700

    Meta tags and styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../app.css';
+	import 'prismjs/themes/prism-tomorrow.css';
 	import { dev } from '$app/environment';
 
 	// we can't import directly because when CSR is disabled it won't run on the client
@@ -8,6 +9,21 @@
 	const builtImport = `<script type="module" src="/__islands/is-land.js"></sc${'ript>'}`;
 	const islandPath = dev ? devImport : builtImport;
 </script>
+
+<svelte:head>
+	<meta property="og:site_name" content="SvelteKit + is-land" />
+	<meta property="og:title" content="SvelteKit + is-land" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://sveltekit-is-land.vercel.app/" />
+	<meta
+		property="og:description"
+		content="A partially-hydrated SvelteKit site using @11ty/is-land"
+	/>
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:creator" content="@geoffrich_" />
+	<meta property="og:image" content="https://sveltekit-is-land.vercel.app/social.png" />
+	<meta name="twitter:image" content="https://sveltekit-is-land.vercel.app/social.png" />
+</svelte:head>
 
 <nav data-sveltekit-prefetch>
 	<a href="/">Home</a>
